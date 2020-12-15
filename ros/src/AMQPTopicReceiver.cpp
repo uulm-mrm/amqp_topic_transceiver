ec4cb5ed96977e1806243619bf642cf05a582f47
Jan Strohbeck <jan.strohbeck@uni-ulm.de>
2020-12-15 10:18:36 +0000

    [UPDATE] some refactoring.

--- ros/src/AMQPTopicReceiver.cpp
+++ ros/src/AMQPTopicReceiver.cpp
@@ -1,81 +1,13 @@
 #include <amqp_topic_transceiver/AMQPTopicReceiver.h>
 #include <topic_tools/shape_shifter.h>
-
-void die(const char* fmt, ...)
-{
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  va_end(ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-void die_on_error(int x, char const* context)
-{
-  if (x < 0)
-  {
-    fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x));
-    exit(1);
-  }
-}
-
-void die_on_amqp_error(amqp_rpc_reply_t x, char const* context)
-{
-  switch (x.reply_type)
-  {
-    case AMQP_RESPONSE_NORMAL:
-      return;
-
-    case AMQP_RESPONSE_NONE:
-      fprintf(stderr, "%s: missing RPC reply type!\n", context);
-      break;
-
-    case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-      fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x.library_error));
-      break;
-
-    case AMQP_RESPONSE_SERVER_EXCEPTION:
-      switch (x.reply.id)
-      {
-        case AMQP_CONNECTION_CLOSE_METHOD:
-        {
-          auto* m = static_cast<amqp_connection_close_t*>(x.reply.decoded);
-          fprintf(stderr,
-                  "%s: server connection error %uh, message: %.*s\n",
-                  context,
-                  m->reply_code,
-                  static_cast<int>(m->reply_text.len),
-                  static_cast<char*>(m->reply_text.bytes));
-          break;
-        }
-        case AMQP_CHANNEL_CLOSE_METHOD:
-        {
-          auto* m = static_cast<amqp_channel_close_t*>(x.reply.decoded);
-          fprintf(stderr,
-                  "%s: server channel error %uh, message: %.*s\n",
-                  context,
-                  m->reply_code,
-                  static_cast<int>(m->reply_text.len),
-                  static_cast<char*>(m->reply_text.bytes));
-          break;
-        }
-        default:
-          fprintf(stderr, "%s: unknown server error, method id 0x%08X\n", context, x.reply.id);
-          break;
-      }
-      break;
-  }
-
-  exit(1);
-}
+#include <amqp_topic_transceiver/utils.h>
 
 DEFINE_LOGGER_VARIABLES
 
 namespace amqp_topic_transceiver
 {
 AMQPTopicReceiver::AMQPTopicReceiver(ros::NodeHandle node_handle, ros::NodeHandle private_node_handle)
-  : private_nh_(private_node_handle), nh_(node_handle)
+  : nh_(node_handle), private_nh_(private_node_handle)
 {
   _setStreamName(ros::this_node::getName());
   _initLogger();
@@ -155,17 +87,33 @@ private:
   int size;
 };
 
+static inline std::string b2s(const amqp_bytes_t& bytes)
+{
+  return std::string(static_cast<const char*>(bytes.bytes), bytes.len);
+}
+
+// static inline std::string str2hex(const std::string &input) {
+//   std::stringstream ss;
+//
+//   ss << std::hex << std::setfill('0');
+//   for (const char c : input) {
+//     ss << std::setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(c));
+//   }
+//
+//   return ss.str();
+// }
+
 bool AMQPTopicReceiver::run()
 {
-  const char* exchange = exchange_.c_str();
-  const char* routingkey = "";
+  amqp_bytes_t exchange = amqp_cstring_bytes(exchange_.c_str());
+  amqp_bytes_t routingkey = amqp_cstring_bytes("");
   amqp_bytes_t queuename = amqp_cstring_bytes("");
 
   auto* res = amqp_queue_declare(conn, 1, queuename, 0, 0, 0, 1, amqp_empty_table);
   queuename = res->queue;
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
 
-  amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchange), amqp_cstring_bytes(""), amqp_empty_table);
+  amqp_queue_bind(conn, 1, queuename, exchange, routingkey, amqp_empty_table);
   die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue to exchange");
 
   amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
@@ -189,16 +137,11 @@ bool AMQPTopicReceiver::run()
     size_t buf_size = envelope.message.body.len;
     LOG_DEB("Received a message with " << buf_size << " bytes!");
 
-    // printf("Delivery %u, exchange %.*s routingkey %.*s\n",
-    //        (unsigned)envelope.delivery_tag, (int)envelope.exchange.len,
-    //        (char *)envelope.exchange.bytes, (int)envelope.routing_key.len,
-    //        (char *)envelope.routing_key.bytes);
-    // printf("----\n");
-    //
-    auto& ct = envelope.message.properties.content_type;
-    std::string content_type = std::string(static_cast<char*>(ct.bytes), ct.len);
-    auto& t = envelope.routing_key;
-    std::string topic = std::string(static_cast<char*>(t.bytes), t.len);
+    // LOG_DEB("Delivery " << envelope.delivery_tag << ", exchange " << b2s(envelope.exchange) << " routingkey " <<
+    // b2s(envelope.routing_key)); LOG_DEB("Message body: " << str2hex(b2s(envelope.message.body)));
+
+    std::string content_type = b2s(envelope.message.properties.content_type);
+    std::string topic = b2s(envelope.routing_key);
 
     LOG_DEB("message content type: " << content_type);
     if (content_type == "application/message-metadata")
@@ -207,9 +150,6 @@ bool AMQPTopicReceiver::run()
       const char* buf = static_cast<const char*>(envelope.message.body.bytes);
       const auto* len_ptr = reinterpret_cast<const uint32_t*>(buf);
       const char* data_ptr = buf + 3 * sizeof(uint32_t);
-      // std::cout << len_ptr[0] << std::endl;
-      // std::cout << len_ptr[1] << std::endl;
-      // std::cout << len_ptr[2] << std::endl;
 
       auto md5sum = std::string(data_ptr, len_ptr[0]);
       auto datatype = std::string(data_ptr + len_ptr[0], len_ptr[1]);
@@ -234,17 +174,6 @@ bool AMQPTopicReceiver::run()
     }
     else
     {
-      // std::cout << md5sum << " | " << datatype << " | " << def << " | " <<
-      // (int)latch << std::endl;
-
-      // printf("Delivery %u, exchange %.*s routingkey %.*s\n",
-      //        (unsigned)envelope.delivery_tag, (int)envelope.exchange.len,
-      //        (char *)envelope.exchange.bytes, (int)envelope.routing_key.len,
-      //        (char *)envelope.routing_key.bytes);
-      // printf("----\n");
-
-      // std::cout << std::string((char *)envelope.message.body.bytes,
-      // envelope.message.body.len) << std::endl;
       LOG_DEB("Got message data on topic " << topic);
 
       auto info_entry = pubs_.find(topic);
--- ros/src/AMQPTopicTransmitter.cpp
+++ ros/src/AMQPTopicTransmitter.cpp
@@ -1,81 +1,13 @@
 #include <amqp_topic_transceiver/AMQPTopicTransmitter.h>
 #include <topic_tools/shape_shifter.h>
-
-static void die(const char* fmt, ...)
-{
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  va_end(ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-static void die_on_error(int x, char const* context)
-{
-  if (x < 0)
-  {
-    fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x));
-    exit(1);
-  }
-}
-
-static void die_on_amqp_error(amqp_rpc_reply_t x, char const* context)
-{
-  switch (x.reply_type)
-  {
-    case AMQP_RESPONSE_NORMAL:
-      return;
-
-    case AMQP_RESPONSE_NONE:
-      fprintf(stderr, "%s: missing RPC reply type!\n", context);
-      break;
-
-    case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-      fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x.library_error));
-      break;
-
-    case AMQP_RESPONSE_SERVER_EXCEPTION:
-      switch (x.reply.id)
-      {
-        case AMQP_CONNECTION_CLOSE_METHOD:
-        {
-          auto* m = static_cast<amqp_connection_close_t*>(x.reply.decoded);
-          fprintf(stderr,
-                  "%s: server connection error %uh, message: %.*s\n",
-                  context,
-                  m->reply_code,
-                  static_cast<int>(m->reply_text.len),
-                  static_cast<char*>(m->reply_text.bytes));
-          break;
-        }
-        case AMQP_CHANNEL_CLOSE_METHOD:
-        {
-          auto* m = static_cast<amqp_channel_close_t*>(x.reply.decoded);
-          fprintf(stderr,
-                  "%s: server channel error %uh, message: %.*s\n",
-                  context,
-                  m->reply_code,
-                  static_cast<int>(m->reply_text.len),
-                  static_cast<char*>(m->reply_text.bytes));
-          break;
-        }
-        default:
-          fprintf(stderr, "%s: unknown server error, method id 0x%08X\n", context, x.reply.id);
-          break;
-      }
-      break;
-  }
-
-  exit(1);
-}
+#include <amqp_topic_transceiver/utils.h>
 
 DEFINE_LOGGER_VARIABLES
 
 namespace amqp_topic_transceiver
 {
 AMQPTopicTransmitter::AMQPTopicTransmitter(ros::NodeHandle node_handle, ros::NodeHandle private_node_handle)
-  : private_nh_(private_node_handle), nh_(node_handle)
+  : nh_(node_handle), private_nh_(private_node_handle)
 {
   _setStreamName(ros::this_node::getName());
   _initLogger();
